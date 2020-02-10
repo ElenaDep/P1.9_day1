910cb6eb644e3402c462fddcf68415acefacdf10
Elena De Paoli <elena.dpaoli@gmail.com>
2020-02-11 00:44:42 +0100

    add usage istructions

--- tab_functions.py
+++ tab_functions.py
@@ -1,18 +1,23 @@
 from __future__ import print_function
 import sys
 
-func_num = int(sys.argv[1])
+if len(sys.argv) == 1 :
+	print("Usage: insert a number as argument to select which function you want to use\n")
+	print("- 1 for f(x)=x\n")
 
-def function1(x):
-	return x
+else:
 
-xval = [n*1.0 for n in range(-5,6,1)]
-
-if func_num == 1:
-	yval=function1(xval)
+	def function1(x):
+		return x
 
 
-import matplotlib.pyplot as plt
-plt.plot(yval)
-plt.show()
+	xval = [n*1.0 for n in range(-5,6,1)]
+
+	func_num = int(sys.argv[1])
+	if func_num == 1:
+		yval=function1(xval)
+
+	import matplotlib.pyplot as plt
+	plt.plot(yval)
+	plt.show()
 
