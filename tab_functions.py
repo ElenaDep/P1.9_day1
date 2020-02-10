dea724e71733abea165dce62ed4a6487ec781726
Elena De Paoli <elena.dpaoli@gmail.com>
2020-02-11 00:24:24 +0100

    add set lists

--- tab_functions.py
+++ tab_functions.py
@@ -2,3 +2,13 @@ from __future__ import print_function
 import sys
 
 func_num = int(sys.argv[1])
+
+def function1(x):
+                return x
+
+        xval = [n*1.0 for n in range(-5,6,1)]
+
+if func_num == 1:
+                yval=function1(xval)
+
+
