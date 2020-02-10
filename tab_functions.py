5e886b85af5bb396327276d725f2cffef93c7289
Elena De Paoli <elena.dpaoli@gmail.com>
2020-02-11 00:50:06 +0100

    add irrational functions

--- tab_functions.py
+++ tab_functions.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import sys
+from math import *
 
 if len(sys.argv) == 1 :
 	print("Usage: insert a number as argument to select which function you want to use\n")
@@ -10,12 +11,25 @@ else:
 	def function1(x):
 		return x
 
+	def irrat_e(xlist):
+		return [ exp(x) for x in xlist ]
+
+
+	def irrat_s(xlist):
+		return [ sqrt(abs(x)) for x in xlist ]
+
 
 	xval = [n*1.0 for n in range(-5,6,1)]
 
 	func_num = int(sys.argv[1])
 	if func_num == 1:
 		yval=function1(xval)
+
+	if func_num == 2:
+		yval=irrat_e(xval)
+
+	if func_num == 3:
+		yval=irrat_s(xval)
 
 	import matplotlib.pyplot as plt
 	plt.plot(yval)
